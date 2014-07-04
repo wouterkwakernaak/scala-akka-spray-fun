f7b29b9d1a1e39c70dd4b68bc997b478be2eac39
Wouter Kwakernaak <wouter@wouterkwakernaak.nl>
2014-07-04 17:06:02 +0200

    requests are now forwarded to an actor

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -1,15 +1,35 @@
+import akka.actor._
+import akka.pattern.AskSupport
+import akka.util.Timeout
+import scala.concurrent.duration._
 import spray.routing._
+import spray.httpx.marshalling._
+import spray.http._
 
-trait Routes extends HttpService {
+trait Routes extends HttpService with AskSupport {
+
+  val twitterActor = actorRefFactory.actorOf((Props[TwitterActor]))
+  implicit val timeout = Timeout(1 seconds)
+  implicit val executionContext = actorRefFactory.dispatcher
 
   val tweetsRoute: Route =
     path("tweets") {
       get {
-        complete("test tweet")
+        onSuccess((twitterActor ? None).mapTo[List[Tweet]]) {
+          tweets => complete(tweets)
+        }
       } ~
         post {
-          complete("hey")
+          val tweet = Tweet("Wouter", "Dummy tweet")
+          onSuccess((twitterActor ? tweet).mapTo[List[Tweet]]) {
+            tweets => complete(tweets)
+          }
         }
     }
+
+  implicit val TweetsMarshaller = Marshaller.of[List[Tweet]](ContentTypes.`text/plain`) { (tweets, contentType, ctx) =>
+    val string = tweets.foldLeft("")((cum, tweet) => cum + tweet.author + ": " + tweet.text + ", ")
+    ctx.marshalTo(HttpEntity(contentType, string))
+  }
 
 }
--- src/test/scala/TwitterServiceSpec.scala
+++ src/test/scala/TwitterServiceSpec.scala
@@ -1,19 +1,18 @@
+import akka.actor.Props
 import org.scalatest.{Matchers, FlatSpec}
 import spray.testkit._
 
 class TwitterServiceSpec extends FlatSpec with Matchers with ScalatestRouteTest with Routes {
 
   def actorRefFactory = system
+  val tweet1 = Tweet("Wouter", "Test tweet 1")
+  val tweet2 = Tweet("Wouter", "Test tweet 2")
+  twitterActor ! tweet1
+  twitterActor ! tweet2
 
-  "The Twitter service" should "return tweets after a GET request" in {
-      Get("/tweets") ~> tweetsRoute ~> check {
-        responseAs[String] should include ("tweet")
-      }
-    }
-
-  it should "return all tweets including the posted one after a POST request" in {
+  "The Twitter service" should "return tweets after a POST request" in {
     Post("/tweets") ~> tweetsRoute ~> check {
-      responseAs[String] should include ("hey")
+      responseAs[String] should include ("tweet")
     }
   }
 
